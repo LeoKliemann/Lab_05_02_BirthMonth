633b59f29d158018b5e47b38d7295ad75aeccb41
LeoKliemann <lgkliemann05@gmail.com>
2026-02-18 17:58:23 -0500

    Finished Task 2

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
+        Scanner in = new Scanner(System.in);
+        int birthMonth = 0;
+        String trash ="";
 
+        System.out.println("Enter your birth month [1-12]");
+
+        if(in.hasNextInt())
+        {
+            birthMonth = in.nextInt();
+            in.nextLine();
+
+            if(birthMonth >= 1 && birthMonth <= 12)
+            {
+                System.out.println("Your birth month is: " + birthMonth);
+            }
+            else
+            {
+                System.out.println("You entered an incorrect month: " + birthMonth);
+            }
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("You said your month was: " + trash);
+            System.out.println("Run again and enter a number value");
+        }
     }
 }
