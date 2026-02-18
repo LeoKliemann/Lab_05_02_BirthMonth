fc842807e91426f6a0b4bd1a57b3490e648b54be
LeoKliemann <lgkliemann05@gmail.com>
2026-02-18 17:36:51 -0500

    Added psuedocode

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,16 @@
+//class BirthMonth
+//	main()
+//		num birthMonth
+//		output “Enter your birth month [1-12]”
+//		input birthMonth
+//		if birthMonth >= 1 and birthMonth =< 12 then
+//			output “Your birth month is:” + birthMonth
+//		else
+//			output “You entered an incorrect month:” + birthMonth
+//		endIf
+//	return
+//endClass
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
